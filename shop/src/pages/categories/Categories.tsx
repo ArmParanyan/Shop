d2b514f0b66a70b7cedfd2797df2f55aa29e2bc6
ArmParanyan <aparanyan472@gmail.com>
2023-12-29 18:39:44 +0400

    Final project, but I didn't make the stars, load more, and orders.

--- shop/src/pages/categories/Categories.tsx
+++ shop/src/pages/categories/Categories.tsx
@@ -1,23 +1,8 @@
-import {useEffect, useState} from "react";
-import {useGetCategories} from "../../hooks/useGetCategories";
-
 export const Categories = () => {
-
-    const [id, setId] = useState<string>("");
-
-    const {data} = useGetCategories(id);
-
-    useEffect(() => {
-        if (data && data.length > 0) {
-            setId(data[0].name);
-        }
-    }, [data]);
-
-    // <CategoriesMenu />
 
     return (
         <div>
-
+            Categories
 
         </div>
     );
